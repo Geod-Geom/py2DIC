0c8ed153a1717c8817f60a7b6a78501603b87a9c
Valeria Belloni <valeria.belloni@uniroma1.it>
2026-05-24 14:49:23 +0200

    Fix syntax for setting CONDAPATH in batch file

--- sources/run_py2DIC.bat
+++ sources/run_py2DIC.bat
@@ -6,7 +6,7 @@ rem - conda to be in the PATH
 rem - cmd.exe to be initialized with conda init
 
 rem Define here the path to your conda installation
-set CONDAPATH="C:\Users\Utente\anaconda3\"
+set "CONDAPATH=C:\Users\Utente\anaconda3\"
 rem Define here the name of the environment
 set ENVNAME=py2DIC
 
